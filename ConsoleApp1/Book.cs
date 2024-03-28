f0abe844118a1b7111940adcfd7643156d9ef5b3
tunay95 <cypherhsynl1@gmail.com>
2024-03-28 14:28:41 +0400

    yetoo

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Book:Product
+    {
+        public string Genre;
+        public Book(string no, double price, string name, string genre)
+        {
+            No = no;
+            Price = price;
+            Name = name;
+            Genre = genre;
+        }
+        public void ShowInfo()
+        {
+
+            Console.WriteLine($"No: {No}, Price: {Price}, Name: {Name}, Genre: {Genre}");
+        }
+    }
+}
+    
---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Product
+    {
+        private double _price;
+        private int _count;
+        public string No;
+        public string Name;
+
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun daxil et");
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _count = value;
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun daxil et");
+
+                }
+            }
+        }
+    }
+}
+    
